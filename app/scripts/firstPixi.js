a1bf1a94dccc95b478e40fa6d1f6b34a4d5be8b5
Devin Loaner <>
2021-03-11 12:43:15 -0800

    init

---
+++ app/scripts/firstPixi.js
@@ -0,0 +1,16 @@
+import * as PIXI from 'pixi.js'
+
+let type = 'WebGL'
+if(!PIXI.utils.isWebGLSupported()) {
+  type = 'canvas'
+}
+
+PIXI.utils.sayHello(type)
+PIXI.utils.sayHello(type)
+
+let app = new PIXI.Application({width: 256, height: 256})
+document.body.appendChild(app.view)
+
+app.renderer.backgroundColor = 0x061639;
+app.renderer.autoResize = true
+app.renderer.resize(512, 512)
