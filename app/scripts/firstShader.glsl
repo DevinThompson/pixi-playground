ba0fdce4a68fa3e6bf245958f135c7506b01d23a
Devin Loaner <>
2021-03-12 12:35:54 -0800

    images and stuff

---
+++ app/scripts/firstShader.glsl
@@ -0,0 +1,5 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,10 +8,66 @@ if(!PIXI.utils.isWebGLSupported()) {
 PIXI.utils.sayHello(type)
 PIXI.utils.sayHello(type)
 
-let app = new PIXI.Application({width: 256, height: 256})
+//Aliases
+let Application = PIXI.Application
+
+let app = new Application({width: 256, height: 256})
+
+let loader = app.loader
+let resources = app.loader.resources
+let Sprite = PIXI.Sprite
+
 document.body.appendChild(app.view)
+
+//Get shader code as a string
+var shaderCode = document.getElementById('shader').innerHTML
+//Create our Pixi filter using our custom shader code
+// var simpleShader = new PIXI.AbstractFilter('',shaderCode);
 
 app.renderer.backgroundColor = 0x061639;
 app.renderer.autoResize = true
-app.renderer.resize(512, 512)
-console.log('hello')
+app.renderer.resize(800, 800)
+console.log('ho')
+
+loader
+  .add('examples/images/cat.png')
+  .add('examples/images/izzy.jpeg')
+  .load(setup);
+
+function setup(){
+  let cat = new Sprite(
+    loader.resources['examples/images/cat.png'].texture
+  )
+
+  let izzy = new Sprite(
+    loader.resources['examples/images/izzy.jpeg'].texture
+  )
+
+  izzy.x = app.renderer.width / 2
+  izzy.y = app.renderer.height / 2
+
+  resizeAspectRatio(300, izzy)
+  izzy.anchor.set(0.5)
+
+  app.stage.addChild(cat)
+  app.stage.addChild(izzy)
+
+  izzy.filters = [simpleShader]
+}
+
+// animate(app.stage)
+
+// function animate(stage) {
+//   requestAnimationFrame(animate)
+
+//   renderer.render(stage)
+// }
+
+function resizeAspectRatio(targetWidth, sprite) {
+  let aspectRatio = sprite.height / sprite.width
+
+  let newHeight = targetWidth * aspectRatio
+
+  sprite.width = targetWidth
+  sprite.height = newHeight
+}
